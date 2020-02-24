0e97ecedde7370f67a2a3cb520bb78e627d2c642
alinaiba <zuicc@qq.com>
2020-02-25 03:30:14 +0800

    个人资料更新

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -36,7 +36,27 @@ class UsersController extends Controller
     }
 
 
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
 
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user->update([
+            'name' => $request->name,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user->id);
+    }
 
 
 
