99ed228a238bf5c73c5893e9ca42f20917e12f71
alinaiba <zuicc@qq.com>
2020-02-27 04:02:43 +0800

    迭代用户中心界面

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,32 +1,42 @@
+
+
+
+
+
+
 @extends('layouts.default')
 @section('title', $user->name)
 
 @section('content')
-    <div class="row">
-        <div class="offset-md-2 col-md-8">
-            <div class="col-md-12">
-                <div class="offset-md-2 col-md-8">
-                    <section class="user_info">
-                        @include('shared._user_info', ['user' => $user])
-                    </section>
+    <div class="col-lg-8 left">
 
 
-                    <section class="bubble">
-                        @if ($bubbles->count() > 0)
-                        <ul class="list-unstyled">
-                            @foreach ($bubbles as $bubble)
-                                @include('bubbles._bubble')
-                            @endforeach
-                        </ul>
-                        <div class="mt-5">
-                            {!! $bubbles->render() !!}
-                        </div>
-                        @else
-                        <p>没有冒泡数据</p>
-                        @endif
-                    </section>
-                </div>
-            </div>
+
+        <div class="part ">
+
+            <span>全部</span>
+            <span>冒泡</span>
+            <span>收藏</span>
+            <span></span>
+            <span></span>
+
         </div>
+
+
+
     </div>
+
+
+
+
+
+
+
+    @include('sidebar._home-sidebar')
+
+
+
+
+
 @stop
+
